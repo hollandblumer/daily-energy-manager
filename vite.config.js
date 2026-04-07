91ebcdc3f31a8bec64c1fcec54f3ecba05051a54
j5d5d5 <j5d5d5bf7a4a0f0a@c-mercor.com>
2026-04-07 14:06:59 -0400

    added energy monitor UI and API integration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // MAKE SURE THIS IS HERE:
+  base: "/daily-energy-manager/",
+});
